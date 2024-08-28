637fb050ddda4028570452698d2212ef7bd4225e
Bhoomi <122143468+Bhoomi1503@users.noreply.github.com>
2024-08-28 12:32:24 +0530

    Updated the comments in Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,19 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
+	    Scanner sc=new Scanner(System.in);      //Scanner class
+	    
+	    //Getting input from the user 
+	    
+	    System.out.print("Enter the Number:");
 	    int num=sc.nextInt();
+	    System.out.print("Enter a number from 1 to 7 to display the weak:");
 	    int week=sc.nextInt();
+	    System.out.print("Enter a number to print a range:");
 	    int tennum=sc.nextInt();
-		sc.close();
+	    
+	    //if else loop
+	    
 	    if(num>0){
 	        System.out.println("Positive");
 	    }
@@ -16,6 +24,9 @@ public class Main
 	    else{
 		    System.out.println("Negative");
 	    }
+	    
+	    //Switch case
+	    
 	    switch(week){
 	        case 1:
 	            System.out.println("Sunday");
@@ -43,10 +54,15 @@ public class Main
 	           
 	    }
 	    
+	    //for loop
+	    
 	    for(int i=1;i<=tennum;i++){
 	        System.out.print(i+" ");
 	    }
 	    System.out.print("\n");
+	    
+	    //while loop 
+	    
 	    int n1=5;
 	    while(n1>0){
 	        
@@ -54,6 +70,9 @@ public class Main
 	        n1--;
 	    }
 	    System.out.print("\n");
+	    
+	    //do while loop
+	    
 	    int n2=1;
 	    do{
 	        System.out.print(n2+" ");
